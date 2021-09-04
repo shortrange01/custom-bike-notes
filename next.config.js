41005c3a8c27dd673a1d6f185387802c6173797f
Hasegawa Taiji <shortrange01@gmail.com>
2021-09-04 20:13:08 +0900

    chore: 環境変数設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    env: {
+        FIREBASE_KEY: process.env.FIREBASE_KEY,
+        FIREBASE_DOMAIN: process.env.FIREBASE_DOMAIN,
+        FIREBASE_DATABASE: process.env.FIREBASE_DATABASE,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+};
